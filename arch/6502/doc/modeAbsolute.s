09d8c1bba260f03a64b5f0c0491bfcafe0871eee
agsb <barcellos.alvaro@gmail>
2023-10-22 16:36:10 -0300

    reviews

--- arch/6502/doc/modeAbsolute.s
+++ arch/6502/doc/modeAbsolute.s
@@ -1,11 +1,11 @@
     
 ;------------------------------------------------------------------------------
 ; using absolute address direct indirect access    
-; pros: 
+; pros_: 
 ;   offsets inline from a fixed reference
-; cons: 
+; cons_: 
 ;   can not change inline reference
-; multitask and multiuser : 
+; multitask and multiuser _: 
 ;   indexed stacks are 128 words, Charles Moore says 22 is enough. 
 ;   Then could split 5 stacks for users or tasks,
 ;   more than must exchange stacks values       
@@ -32,297 +32,303 @@
 ; uses 4 bytes in memory for internal use
 ;
 
+.segment "ZERO"
+
 ; zero page 
 tos:    .word $0
 nos:    .word $0
 
+.segment "CODE"
+
 ; any page
 
-ips:    .byte $0
-ipr:    .byte $0
+isp:    .byte $0
+irp:    .byte $0
 
 aps:    .word $0
 apr:    .word $0
+
+FALSE = 0
 
 ;------------------------------------------------------------------------------
 ;   data stack
 ;------------------------------------------------------------------------------
 
-lose: ; to pull 
+lose_: ; to pull 
     ; ldx isp
     inx
     inx
     stx isp
-    rst
+    rts
 
-keep: ; to push 
+keep_: ; to push 
     ; ldx isp
     dex
     dex
     stx isp
-    rst
+    rts
 
-pull:
+pull_:
     ldx isp
-    lda asp + 0, x
+    lda aps + 0, x
     sta tos + 0
-    lda asp + 1, x
+    lda aps + 1, x
     sta tos + 1
-    jmp drop
+    jmp drop_
 
-push:
+push_:
     ldx isp
     lda tos + 0
-    sta asp - 2, x
+    sta aps - 2, x
     lda tos + 1
-    sta asp - 1, x
-    jmp keep
+    sta aps - 1, x
+    jmp keep_
  
-push2:
+push2_:
     ldx isp
     lda nos + 0
-    sta asp - 4, x
+    sta aps - 4, x
     lda nos + 1
-    sta asp - 3, x
+    sta aps - 3, x
     lda tos + 0
-    sta asp - 2, x
+    sta aps - 2, x
     lda tos + 1
-    sta asp - 1, x
-    jsr keep
-    jmp keep
+    sta aps - 1, x
+    jsr keep_
+    jmp keep_
  
-pull2:
+pull2_:
     ldx isp
-    lda asp + 0, x
+    lda aps + 0, x
     sta tos + 0
-    lda asp + 1, x
+    lda aps + 1, x
     sta tos + 1
-    lda asp + 2, x
+    lda aps + 2, x
     sta nos + 0
-    lda asp + 3, x
+    lda aps + 3, x
     sta nos + 1
-    jsr drop
-    jmp drop
+    jsr drop_
+    jmp drop_
    
-drop:  
+drop_:  
     ldx isp 
-    jmp lose
+    jmp lose_
 
-dup:
+dup_:
     ldx isp
-    lda asp + 0, x
-    sta asp - 2 
-    lda asp + 1, x
-    sta asp - 1
-    jmp keep
+    lda aps + 0, x
+    sta aps - 2 
+    lda aps + 1, x
+    sta aps - 1
+    jmp keep_
 
-over:
+over_:
     ldx isp
-    lda asp + 2, x
-    sta asp - 2 
-    lda asp + 3, x
-    sta asp - 1
-    jmp keep
+    lda aps + 2, x
+    sta aps - 2 
+    lda aps + 3, x
+    sta aps - 1
+    jmp keep_
 
-swap:
+swap_:
     ldx isp
-    lda asp + 0, x
-    sta asp - 2 
-    lda asp + 1, x
-    sta asp - 1
-    lda asp + 2, x
-    sta asp + 0 
-    lda asp + 3, x
-    sta asp + 1
-    lda asp - 2, x
-    sta asp + 2 
-    lda asp - 1, x
-    sta asp + 3
+    lda aps + 0, x
+    sta aps - 2 
+    lda aps + 1, x
+    sta aps - 1
+    lda aps + 2, x
+    sta aps + 0 
+    lda aps + 3, x
+    sta aps + 1
+    lda aps - 2, x
+    sta aps + 2 
+    lda aps - 1, x
+    sta aps + 3
     rts
 
-rot:
+rot_:
     ldx isp
-    lda asp + 4, x
-    sta asp - 2 
-    lda asp + 5, x
-    sta asp - 1
-    lda asp + 2, x
-    sta asp + 4 
-    lda asp + 3, x
-    sta asp + 5
-    lda asp + 0, x
-    sta asp + 2 
-    lda asp + 1, x
-    sta asp + 3
-    lda asp - 2, x
-    sta asp + 0 
-    lda asp - 1, x
-    sta asp + 1
+    lda aps + 4, x
+    sta aps - 2 
+    lda aps + 5, x
+    sta aps - 1
+    lda aps + 2, x
+    sta aps + 4 
+    lda aps + 3, x
+    sta aps + 5
+    lda aps + 0, x
+    sta aps + 2 
+    lda aps + 1, x
+    sta aps + 3
+    lda aps - 2, x
+    sta aps + 0 
+    lda aps - 1, x
+    sta aps + 1
     rts
 
-and:
+and_:
     ldx isp
-    lda asp + 0, x
-    and asp + 2, x
-    sta asp + 2, x 
-    lda asp + 1, x 
-    and asp + 3, x
-    sta asp + 3, x
-    jmp drop
+    lda aps + 0, x
+    and aps + 2, x
+    sta aps + 2, x 
+    lda aps + 1, x 
+    and aps + 3, x
+    sta aps + 3, x
+    jmp drop_
 
-or:
+or_:
     ldx isp
-    lda asp + 0, x
-    ora asp + 2, x
-    sta asp + 2, x
-    lda asp + 1, x
-    ora asp + 3, x
-    sta asp + 3, x
-    jmp drop
+    lda aps + 0, x
+    ora aps + 2, x
+    sta aps + 2, x
+    lda aps + 1, x
+    ora aps + 3, x
+    sta aps + 3, x
+    jmp drop_
 
-xor:
+xor_:
     ldx isp
-    lda asp + 0, x
-    eor asp + 2, x
-    sta asp + 2, x
-    lda asp + 1, x
-    eor asp + 3, x
-    sta asp + 3, x
-    jmp drop
+    lda aps + 0, x
+    eor aps + 2, x
+    sta aps + 2, x
+    lda aps + 1, x
+    eor aps + 3, x
+    sta aps + 3, x
+    jmp drop_
 
-cpt:
+cpt_:
     ldx isp
     sec
     tya
-    sbc asp + 0, x
-    sta asp + 0, x
+    sbc aps + 0, x
+    sta aps + 0, x
     sec
     tya
-    sbc asp + 1, x
-    sta asp + 1, x
+    sbc aps + 1, x
+    sta aps + 1, x
     rts
 
-neg:
+neg_:
     lda #$00
     tay
-    jmp cpt
+    jmp cpt_
 
-inv: 
+inv_: 
     lda #$FF
     tay
-    jmp cpt
+    jmp cpt_
 
-sub:
+sub_:
     ldx isp
     sec
-    lda asp + 2, x
-    sbc asp + 0, x
-    sta asp + 2, x
-    lda asp + 3, x
-    sbc asp + 1, x
-    sta asp + 3, x
-    jmp drop
+    lda aps + 2, x
+    sbc aps + 0, x
+    sta aps + 2, x
+    lda aps + 3, x
+    sbc aps + 1, x
+    sta aps + 3, x
+    jmp drop_
 
-add:
+add_:
     ldx isp
     clc
-    lda asp + 2, x
-    adc asp + 0, x
-    sta asp + 2, x
-    lda asp + 3, x
-    adc asp + 1, x
-    sta asp + 3, x
-    jmp drop
+    lda aps + 2, x
+    adc aps + 0, x
+    sta aps + 2, x
+    lda aps + 3, x
+    adc aps + 1, x
+    sta aps + 3, x
+    jmp drop_
 
-cmp:
+cmp_:
     ldx isp
     sec
-    lda asp + 2, x
-    sbc asp + 0, x
-    lda asp + 3, x
-    sbc asp + 1, x
+    lda aps + 2, x
+    sbc aps + 0, x
+    lda aps + 3, x
+    sbc aps + 1, x
     rts
 
-eq:
-    jsr cmp
-    beq true2
-    bne false2
+eq_:
+    jsr cmp_
+    beq true2_
+    bne false2_
 
-lt:
-    jsr cmp
-    bmi true2
-    bpl false2
+lt_:
+    jsr cmp_
+    bmi true2_
+    bpl false2_
 
-gt:
-    jsr cmp
-    bmi false2
-    beq false2
-    bpl true2
+gt_:
+    jsr cmp_
+    bmi false2_
+    beq false2_
+    bpl true2_
 
-same2:
-    sta asp + 2, x
-    sta asp + 3, x
-    jmp drop
+same2_:
+    sta aps + 2, x
+    sta aps + 3, x
+    jmp drop_
 
-false2:
+false2_:
     ldx isp
-    lda FALSE
-    beq same2
+    lda #(FALSE)
+    beq same2_
 
-true2:
+true2_:
     ldx isp
-    lda FALSE - 1
-    bne same2
+    lda #(!FALSE)
+    bne same2_
 
-shl:
+shl_:
     ldx isp
-    als asp + 0, x
-    rol asp + 1, x
+    asl aps + 0, x
+    rol aps + 1, x
     rts
 
-shr:
+shr_:
     ldx isp
-    lsr asp + 0, x
-    ror asp + 1, x
+    lsr aps + 0, x
+    ror aps + 1, x
     rts
 
-cto:
-    jsr take2
+cto_:
+    jsr pull2_
     ldy #0
     lda nos + 0
     sta (tos), y
     rts
 
-to:
-    jsr cto
+to_:
+    jsr cto_
     iny
     lda nos + 1
     sta (tos), y
     rts
 
-cat:
+cat_:
     ldx isp
-    lda asp + 0, x
+    lda aps + 0, x
     sta tos + 0
-    lda asp + 1, x
+    lda aps + 1, x
     sta tos + 1
     ldy #0
     lda (tos), y
-    sta asp + 0, x
+    sta aps + 0, x
     rts
 
-at:
-    jsr cat
+at_:
+    jsr cat_
     iny 
     lda (tos), y
-    sta asp + 1, x
+    sta aps + 1, x
     rts
 
-goto:
-    jsr pull
+goto_:
+    jsr pull_
     jmp (tos)
 
-exec:
+exec_:
     pla
     sta tos + 0
     pla
@@ -333,55 +339,53 @@ exec:
 ;   return stack
 ;------------------------------------------------------------------------------
 
-rlose: ; to pull 
+rlose_: ; to pull 
     ; ldx isp
     inx
     inx
     stx isp
-    rst
+    rts
 
-
-rkeep: ; to push 
+rkeep_: ; to push 
     ; ldx isp
     dex
     dex
     stx irp
-    rst
+    rts
 
-rpull:
+rpull_:
     ldx irp
-    lda arp + 0, x
+    lda apr + 0, x
     sta tos + 0
-    lda arp + 1, x
+    lda apr + 1, x
     sta tos + 1
-    jmp rlose
+    jmp rlose_
 
-rpush:
+rpush_:
     ldx irp
     lda tos + 0
-    sta arp - 2, x
+    sta apr - 2, x
     lda tos + 1
-    sta arp - 1, x
-    jmp rkeep
+    sta apr - 1, x
+    jmp rkeep_
  
-rshow:
-    jsr rpull
-    jsr rpush
-    jsr spush
+rshow_:
+    jsr rpull_
+    jsr rpush_
+    jsr push_
     rts
 
-r2s:
-    jsr rpull
-    jsr spush
+r2s_:
+    jsr rpull_
+    jsr push_
     rts
 
-s2r:
-    jsr spull
-    jsr rpush
+s2r_:
+    jsr pull_
+    jsr rpush_
     rts
 
-exit:
-    jsr rpull
+exit_:
+    jsr rpull_
     jmp (tos)
 
-    
